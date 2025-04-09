4b3f8e60a1a5ea78b555728b7184adc9831f0493
Ramiro Caso <caso.ramiro@gmail.com>
2025-04-09 18:34:26 -0300

    Change tabs to spaces

--- include/formatter.hpp
+++ include/formatter.hpp
@@ -14,11 +14,11 @@ namespace iif_sadaf::talk::QMLExpression {
  * @brief Provides string formatting for QML expressions.
  */
 struct Formatter {
-	std::string operator()(std::shared_ptr<UnaryNode> expr) const;
-	std::string operator()(std::shared_ptr<BinaryNode> expr) const;
-	std::string operator()(std::shared_ptr<QuantificationNode> expr) const;
-	std::string operator()(std::shared_ptr<PredicationNode> expr) const;
-	std::string operator()(std::shared_ptr<IdentityNode> expr) const;
+    std::string operator()(std::shared_ptr<UnaryNode> expr) const;
+    std::string operator()(std::shared_ptr<BinaryNode> expr) const;
+    std::string operator()(std::shared_ptr<QuantificationNode> expr) const;
+    std::string operator()(std::shared_ptr<PredicationNode> expr) const;
+    std::string operator()(std::shared_ptr<IdentityNode> expr) const;
 };
 
 std::string format(const Expression& expr);
